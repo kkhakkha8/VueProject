90660db1bb5c91622c0500783ef79f82c4234a8d
DESKTOP-1NJKFD7\linn star <kkhakkha8@gmail.com>
2022-02-25 12:15:42 +0600

    Vue_1_vue_overview

---
+++ vueWebsite.js
@@ -0,0 +1,19 @@
+Vue Website
+
+- Vue is used to create the whole website  with multiple pages & components
+
+-These website are normally called single page application
+
+-All routing is done in the browser & not on the server
+
+
+Single Page application
+
+- Only a single HTML page sent (initially) to the browser
+
+-Vue intercept subsequent requests and handles 'page' changes in the browser
+ by swapping what components are shown on the page
+
+ -Results in a much faster and smoother website experience 
+
+ 
