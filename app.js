10f4cc131fea11544a5e139587bc031ba9a4ed74
DESKTOP-1NJKFD7\linn star <kkhakkha8@gmail.com>
2022-02-25 18:24:50 +0600

    create app and dynamic data

---
+++ app.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    //template:"<h2>Hi There</h2>"
+    data(){
+        return {
+            title:"book",
+            age:33,
+            author:"Hla Aung Khan"
+        }
+    }
+});
+
+app.mount('#app');
