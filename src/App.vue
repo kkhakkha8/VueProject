a3f206631d0dd11f1e26c21130b4c32e799e4555
DESKTOP-1NJKFD7\linn star <kkhakkha8@gmail.com>
2022-03-06 14:39:33 +0600

    ep6_event_emit_and_event_modifier

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <h1 ref="greet">Hi</h1>
-   <Model_com :header="header" :content="content" :theme="theme"/>
+    <div v-if="showModal">
+        <Model_com :header="header" :content="content" :theme="theme" @close="showModal=false" />
+    </div>
+    <button @click="showModal=true">show modal</button>
 </template>
 
 <script>
@@ -10,7 +13,9 @@ export default {
         return {
             header: "login success",
             content: "Welcome User Hla Aung Khan",
-            theme: "success"
+            theme: "success",
+            showModal:false
+            
         }
     },
     components: {
--- src/components/Model_com.vue
+++ src/components/Model_com.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="backdrop">
+  <div class="backdrop" @click.self="closeModal"> 
       <div class="modal" :class="{success:theme==='success',danger:theme==='delete'}">
           <h1>{{header}}</h1>
           <p>{{content}}</p>
@@ -9,7 +9,13 @@
 
 <script>
 export default {
-    props:['header','content','theme']
+    props:['header','content','theme'],
+    methods:{
+        closeModal(){
+            this.$emit('close');
+        }
+    }
+    
 }
 </script>
 
